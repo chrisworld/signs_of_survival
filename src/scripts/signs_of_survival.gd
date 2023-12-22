b7fc286512103eff39f13a92f9627017cc6b9541
Ewald Teuschl <ete@lumpi-berndorf.at>
2023-12-22 17:04:00 +0100

    different bgm for gameplay win and credits

--- src/scripts/signs_of_survival.gd
+++ src/scripts/signs_of_survival.gd
@@ -8,6 +8,9 @@ extends Node2D
 @onready var title_canvas = $title_canvas
 @onready var credits_canvas = $credits_canvas
 @onready var win_canvas = $win_canvas
+@onready var bgm_win = $bgm_win
+@onready var bgm_main_game_loop = $bgm_main_game_loop
+@onready var bgm_credits = $bgm_credits
 
 # playing flag
 var is_playing = false
@@ -101,6 +104,9 @@ func credits_canvas_on():
 	# pause
 	sign_world.pause_world()
 
+	bgm_main_game_loop.stop()
+	if not bgm_win.playing:
+		bgm_credits.play()
 
 func credits_canvas_off():
 
@@ -155,6 +161,9 @@ func _on_credits_canvas_end_credits():
 	title_canvas_on()
 	credits_canvas_off()
 	
+	bgm_win.stop()
+	bgm_credits.stop()
+	bgm_main_game_loop.play()
 
 func _on_sign_world_loose_cutscene_full_dark():
 	print("you lost -> go to title")
@@ -167,6 +176,8 @@ func _on_sign_world_win_cutscene_full_dark():
 	title_canvas_off()
 	#credits_canvas_on()
 	win_canvas_on()
+	bgm_main_game_loop.stop()
+	bgm_win.play()
 
 
 func _on_pause_canvas_go_to_title():
